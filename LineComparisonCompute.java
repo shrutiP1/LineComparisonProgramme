d6922336fcb6132f977923583c189a3dedf7798e
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-07 01:34:13 +0530

    Welcome

---
+++ LineComparisonCompute.java
@@ -0,0 +1,10 @@
+package com.bridegelabz.linecomparison;
+
+public class LineComparisonCompute 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Comparison Computation Program ");	
+	}
+
+}
