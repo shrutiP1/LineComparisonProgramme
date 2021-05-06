b1d0d93e03eda75618753fd7b3d79dd01d2dcb0c
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-07 01:52:25 +0530

    ADD: Check Lines Equal Or Not.

--- LineComparisonCompute.java
+++ LineComparisonCompute.java
@@ -6,6 +6,8 @@ public class LineComparisonCompute
 	static int x2=40;
 	static int y1=30;
 	static int y2=40;
+	int length1=0;
+	int length2=0;
 	public void calLengthUC1()
 	{
 		System.out.println("UC1 output:");
@@ -13,12 +15,30 @@ public class LineComparisonCompute
 		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1), 2));
 		System.out.println("Length : "+length);
 	}
+	public void checkEqualUC2()
+	{
+		System.out.println("UC2 output:");
+		 length1=x2-x1;
+		 length2=y2-y1;
+		 
+		 if(length1==length2)
+		 {
+			 System.out.println("two lines are equal");
+		 }
+		 else
+		 {
+			 System.out.println("Two lines are not equal");
+		 }
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Comparison Computation Program ");
 		//UC1
 		LineComparisonCompute l1=new LineComparisonCompute();
 		l1.calLengthUC1();
+		//UC2
+		l1.checkEqualUC2();
+		
 	}
 
 }
